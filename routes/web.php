87de90bc839b6a802575fadfb4c590385fb59214
soidoia <s.ito33@icloud.com>
2023-07-08 23:40:18 +0900

    ペジネーションまで

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,4 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function() {
-    return view('posts.index');
-});
+Route::get('/', [PostController::class, 'index']);
